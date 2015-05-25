fb7601d7b13e51266aa33649ab748399e718a79b
Mary Wallace <wallaceemee@gmail.com>
2015-05-25 14:49:12 -0700

    add and list functions

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,60 @@
-var newTodoList = function() {
-	// ???
+// var newTodoList = function() {
+// };  
+
+var TodoList = function(){
+  this.collection = [];
+};
+
+TodoList.prototype.add = function(item){
+  return this.collection.push(item);
+};
+
+TodoList.prototype.list = function(){
+  return this.collection;
 };
 
 
 
 // Driver code
 
+// var todoList = newTodoList(); 
 
-var todoList = newTodoList();
+var groceryList = new TodoList(); //newTodoList();
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+
+// tasks is now an array of Task objects
+groceryList.tasks //-> [Task, Task, Task]
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: false}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
+
+
+// getting a task object
+var breadTask = groceryList.tasks[0];
+
+breadTask.id //-> 1 (some unique numerical ID)
+breadTask.description //-> 'bread'
+breadTask.completed //-> false
+
+
+// This should complete the task
+breadTask.complete();
+
+breadTask.completed //-> true
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: true}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
+
+
+// This should remove the task from the todo list
+// breadTask.remove();
+
+groceryList.list();
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
